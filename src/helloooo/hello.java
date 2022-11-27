c63d8975716dcf26d99be38a1256f4e33e584135
yoona <yoona@localhost>
2022-11-27 23:30:47 +0900

    initial commit

---
+++ src/helloooo/hello.java
@@ -0,0 +1,7 @@
+package helloooo;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yoona
+ *
+ */
+module helloooo {
+}
